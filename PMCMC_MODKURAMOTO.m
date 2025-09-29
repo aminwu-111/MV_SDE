81867fc6d93d36be449a0705735933aa3529b035
aminwu-111 <amin.wu@kaust.edu.sa>
2025-09-29 17:15:42 +0300

    Add files via upload

---
+++ PMCMC_MODKURAMOTO.m
@@ -0,0 +1,239 @@
+% This code is PMCMC for the modified kuramoto model 
+
+close all;
+clear;
+clc;
+format long
+
+
+particle_count = 50;
+x0 = 1;
+X0 = ones(particle_count, 1);
+kuramoto_obs = readmatrix(['Modkuramoto_obs_T_100.txt']);
+T = 100;
+
+Lmin = 5;
+LP = 5;
+%sigma_obs = 1;
+% discretization size
+hl = zeros(LP - Lmin + 1, 1);
+hl(1) = 2^(-Lmin);
+for l = 1:LP- Lmin
+    hl(l+1) = 2^(-l- Lmin);
+end
+
+%number of iterations for each level
+Nl = 5000;
+%store the acceptance rate
+Aln = zeros(1, 1);
+Theta_trace = cell(LP - Lmin + 1, 1);
+Theta_trace_1 = cell(LP - Lmin,1);
+Theta_trace_2 = cell(LP - Lmin,1);
+
+Theta_traceN = cell(LP - Lmin + 1, 1);
+Theta_trace_1N = cell(LP - Lmin,1);
+Theta_trace_2N = cell(LP - Lmin,1);
+
+%mean of theta over iterations
+ML_Theta_trace = cell(LP - Lmin + 1, 1);
+%weights for finer and corse level        
+H1_trace = cell(LP - Lmin, 1);
+H2_trace = cell(LP - Lmin, 1);
+
+for k = 1 : LP - Lmin + 1
+    Theta_trace{k, 1} = zeros(Nl(k),3);
+    Theta_traceN{k,1} = zeros(Nl(k),3);
+    ML_Theta_trace{k, 1} = zeros(Nl(k),3);
+end
+
+for i = 1:LP - Lmin
+    Theta_trace_1{i,1} = zeros(Nl(i+1),3);
+    Theta_trace_2{i,1} = zeros(Nl(i+1),3);
+
+    Theta_trace_1N{i,1} = zeros(Nl(i+1),3);
+    Theta_trace_2N{i,1} = zeros(Nl(i+1),3);
+
+    H1_trace{i,1} = zeros(Nl(i+1),1);
+    H2_trace{i,1} = zeros(Nl(i+1),1);
+end
+
+
+delta = 2^(-LP);
+Theta_A = [0,-2,1];
+tic;
+Theta_A_p = Theta_A;
+Theta_SIG_p = [Theta_A_p(1), exp(Theta_A_p(2)),exp(Theta_A_p(3))];
+X_measure = simulate_discrete_modkuramoto_p(delta, T, X0, Theta_SIG_p(1), Theta_SIG_p(2));
+Z =  particle_filter(kuramoto_obs, X_measure, delta, T, X0, Theta_SIG_p(1), Theta_SIG_p(2),  Theta_SIG_p(3));
+lZ = Z;
+l_pos_Theta_A_p = l_posterior(Theta_A_p, lZ);
+Theta_trace{1,1}(1,:) = Theta_A_p;
+
+N_count_1 = 0;
+N_count_last_1 = 0;
+%Sigma_A1 = 0.1*diag([0.5,3,3.75]);
+Sigma_A1 = 0.1*diag([1,3,3.5]);
+for iter = 1:Nl(1)
+ 
+    if mod(iter, 100) == 0
+        fprintf('iter = %f\n', iter);
+        disp(['current AC = ', num2str(N_count_1/(iter))]);
+        disp(['current new AC = ', num2str((N_count_1 - N_count_last_1)/(mod(iter,50)+1))]);
+        disp(['current estimate = [', num2str(Theta_A_p(1)), ', ', num2str(Theta_A_p(2)),', ', num2str(Theta_A_p(3)),  ']']);
+        disp(['estimated posterior = ', num2str(l_pos_Theta_A_p)]);
+    end
+    if mod(iter, 100) == 0
+        N_count_last_1 = N_count_1;
+    end
+    
+    Theta_A_prime_1 = Theta_A_p;
+    Theta_A_prime = mvnrnd(Theta_A_prime_1, Sigma_A1*Sigma_A1');
+    Theta_SIG_prime = [Theta_A_prime(1),exp(Theta_A_prime(2)), exp(Theta_A_prime(3))];
+
+    X_measure_prime = simulate_discrete_modkuramoto_p(delta, T, X0, Theta_SIG_prime(1), Theta_SIG_prime(2));
+    Z_prime =  particle_filter(kuramoto_obs, X_measure_prime, delta, T, X0, Theta_SIG_prime(1), Theta_SIG_prime(2), Theta_SIG_prime(3));
+    lZ_prime = Z_prime;
+    l_pos_Theta_A_prime = l_posterior(Theta_A_prime, lZ_prime);
+ 
+    alpha_U = min(0, l_pos_Theta_A_prime - l_pos_Theta_A_p);
+    U = log(rand);
+    
+    if U < alpha_U
+        Theta_A_p = Theta_A_prime;
+        Theta_SIG_p = Theta_SIG_prime;
+        X_measure = X_measure_prime;
+        lZ = lZ_prime;
+        l_pos_Theta_A_p = l_pos_Theta_A_prime;
+        Theta_trace{1, 1}(iter,:) = Theta_A_prime; 
+        N_count_1 = N_count_1 + 1;        
+    else
+        Theta_trace{1, 1}(iter,:) = Theta_A_p; 
+        lZ = particle_filter(kuramoto_obs, X_measure, delta, T, X0, Theta_SIG_p(1), Theta_SIG_p(2),Theta_SIG_p(3) );
+        l_pos_Theta_A_p2 = l_posterior(Theta_A_p, lZ);  
+        if true && (l_pos_Theta_A_p2 ~= -inf)
+            l_pos_Theta_A_p = l_pos_Theta_A_p2;
+        end        
+    end 
+
+end
+
+Aln = N_count_1 / Nl(1);
+burnin =1;
+Theta_iters = Theta_trace{1,1} ;
+burnin = 1;
+niter = 5000;
+desired_height = 0.12;
+figure_distance = 400;
+f = figure;
+f.Position = f.Position+[0 -figure_distance 0 figure_distance];
+
+ax = subplot(3,1,1);
+plot(burnin:3:niter,Theta_iters(burnin:3:end,1), 'r-',LineWidth=1);
+title('\theta');
+current_height = ax.Position(4);
+remaning_height = desired_height - current_height;
+ax.Position = ax.Position + [0 -remaning_height/2 0 remaning_height/2];
+ylabel('convergence value');
+xlabel('iterations');
+
+ax = subplot(3,1,2);
+plot(burnin:3:niter,Theta_iters(burnin:3:end,2), 'r-',LineWidth=1);
+title('log(\sigma)');
+current_height = ax.Position(4);
+remaning_height = desired_height - current_height;
+ax.Position = ax.Position + [0 -remaning_height/2 0 remaning_height/2];
+ylabel('convergence value');
+xlabel('iterations');
+
+
+ax = subplot(3,1,3);
+plot(burnin:3:niter,Theta_iters(burnin:3:end,3), 'r-',LineWidth=1);
+title('log(\tau) ')
+current_height = ax.Position(4);
+remaning_height = desired_height - current_height;
+ax.Position = ax.Position + [0 -remaning_height/2 0 remaning_height/2];
+ylabel('convergence value');
+xlabel('iterations');
+
+
+
+function X = simulate_discrete_modkuramoto_p(delta_t, T, X0, theta, sigma)
+    particle_count = length(X0);     
+    steps_count = round(T/delta_t);
+    X = zeros(particle_count, steps_count+1);
+    X(:,1) = X0;
+    delta_W = sqrt(delta_t)*randn(particle_count, steps_count);
+    for i = 1:steps_count
+        %{
+        if mod(i,100) == 0
+            disp(['i = ', num2str(i)])
+        end
+        %}
+        X(:,i+1) = X(:,i) + (theta + mean(sin(X(:,i) - X(:,i)'),2)) * delta_t + ...
+                          + sigma/(1+ X(:,i).^2) * delta_W(:,i);
+    end
+end
+
+ 
+function z = particle_filter(Y, X_measure, delta_t, T, X0, theta, sigma, sigma_obs)
+    particle_count = length(X0);    
+    steps_count = round(T/delta_t);
+    X = zeros(particle_count, steps_count+1);
+    X(:,1) = X0;
+    delta_W = sqrt(delta_t)*randn(particle_count, steps_count);
+    log_w = zeros(particle_count, 1);
+    k = 1;
+    lGL_star = zeros(1,T);
+    for i = 2:steps_count+1
+        X(:,i) = X(:,i-1) + (theta + mean(sin(X(:,i-1) - X_measure(:,i-1)'),2)) * delta_t + ...
+                          + sigma/(1+ X(:,i).^2) * delta_W(:,i-1);       
+        if i == round(k/delta_t)+1
+            log_w = log_normpdf(X(:,i), Y(k), sigma_obs);
+            W = exp(log_w - max(log_w));
+            lGL_star(1,k)= log(sum(W)) + max(log_w);
+            W = W / sum(W);
+            if 1/sum(W.^2) <= particle_count
+                I = resampleSystematic(W);
+                X = X(I,:);
+            end
+            k = k + 1;
+        end
+    end
+    z = T * log(1/particle_count) + sum(lGL_star);
+end
+
+function lw = lG(y, x, Cov)
+    k = size(x,2);
+    lw  = -log(sqrt((2*pi)^k * det(Cov))) - 0.5*diag((y-x) * Cov^(-1) * (y-x)') ;
+end
+
+function lpos_p = l_posterior(Theta, lik)
+    log_lik = lik;
+    log_prior = lG(Theta(1),0,0.1) + lG(Theta(2), -1.6, 0.001) + lG(Theta(3), 0, 0.01) ;
+    lpos_p = log_lik + log_prior;
+    
+end
+
+function  indx  = resampleSystematic(w)
+    N = length(w);
+    Q = cumsum(w);
+    indx = zeros(1,N);
+    T = linspace(0,1-1/N,N) + rand(1)/N;
+    T(N+1) = 1;
+    i=1;
+    j=1;
+    while (i<=N)
+        if (T(i)<Q(j))
+            indx(i)=j;
+            i=i+1;
+        else
+            j=j+1;        
+        end
+    end
+end
+
+function a = log_normpdf(x,m,s)
+    a = -log(2*pi)/2 - log(s) - (x-m).^2/(2*s^2);
+    %a = -(x-m).^2/(2*s^2);
+end
+
